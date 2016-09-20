300819a2cb435e493f3927263eccd818b4c93e9d
nekno <nekno@users.noreply.github.com>
2016-09-20 10:54:19 -0500

    Initial commit. Adding files.

---
+++ Transcoder/MainForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Transcoder
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.mainFormBindingSource = new System.Windows.Forms.BindingSource(this.components);
+			this.transcoderFilesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+			this.filePathDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.folderDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.mainFormBindingSource)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.transcoderFilesBindingSource)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToOrderColumns = true;
+			this.dataGridView1.AutoGenerateColumns = false;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.filePathDataGridViewTextBoxColumn,
+            this.folderDataGridViewTextBoxColumn});
+			this.dataGridView1.DataSource = this.transcoderFilesBindingSource;
+			this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.Size = new System.Drawing.Size(719, 269);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// mainFormBindingSource
+			// 
+			this.mainFormBindingSource.DataSource = typeof(Transcoder.MainForm);
+			// 
+			// transcoderFilesBindingSource
+			// 
+			this.transcoderFilesBindingSource.DataMember = "TranscoderFiles";
+			this.transcoderFilesBindingSource.DataSource = this.mainFormBindingSource;
+			// 
+			// filePathDataGridViewTextBoxColumn
+			// 
+			this.filePathDataGridViewTextBoxColumn.DataPropertyName = "FilePath";
+			this.filePathDataGridViewTextBoxColumn.HeaderText = "FilePath";
+			this.filePathDataGridViewTextBoxColumn.Name = "filePathDataGridViewTextBoxColumn";
+			this.filePathDataGridViewTextBoxColumn.ReadOnly = true;
+			// 
+			// folderDataGridViewTextBoxColumn
+			// 
+			this.folderDataGridViewTextBoxColumn.DataPropertyName = "Folder";
+			this.folderDataGridViewTextBoxColumn.HeaderText = "Folder";
+			this.folderDataGridViewTextBoxColumn.Name = "folderDataGridViewTextBoxColumn";
+			this.folderDataGridViewTextBoxColumn.ReadOnly = true;
+			// 
+			// MainForm
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(744, 349);
+			this.Controls.Add(this.dataGridView1);
+			this.Name = "MainForm";
+			this.Text = "Transcoder";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.mainFormBindingSource)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.transcoderFilesBindingSource)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn filePathDataGridViewTextBoxColumn;
+		private System.Windows.Forms.DataGridViewTextBoxColumn folderDataGridViewTextBoxColumn;
+		private System.Windows.Forms.BindingSource transcoderFilesBindingSource;
+		private System.Windows.Forms.BindingSource mainFormBindingSource;
+	}
+}
+
---
+++ Transcoder/MainForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Transcoder
+{
+	public partial class MainForm : Form
+	{
+		public BindingList<TranscoderFile> TranscoderFiles { get; set; }
+
+		public MainForm() {
+			InitializeComponent();
+
+			Load += MainForm_Load;
+			DragEnter += MainForm_DragEnter;
+			DragDrop += MainForm_DragDrop;
+		}
+
+		void MainForm_Load(object sender, EventArgs e) {
+			TranscoderFiles = new BindingList<TranscoderFile>();
+			dataGridView1.DataSource = TranscoderFiles;
+		}
+
+		void MainForm_DragEnter(object sender, DragEventArgs e) {
+			e.Effect = e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.Copy : DragDropEffects.None;
+		}
+
+		async void MainForm_DragDrop(object sender, DragEventArgs e) {
+			var paths = e.Data.GetData(DataFormats.FileDrop) as String[];
+
+			await Task.Run(() => {
+				var tfiles = new List<TranscoderFile>(paths.Length * 15);
+
+				foreach (var path in paths) {
+					if (Directory.Exists(path)) {
+						var files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+						tfiles.AddRange(files.Select(file => new TranscoderFile() { FilePath = file, Folder = path }));
+					} else if (File.Exists(path)) {
+						tfiles.Add(new TranscoderFile() { FilePath = path, Folder = Path.GetDirectoryName(path) });
+					}
+				};
+
+				AddFiles(tfiles);
+			});
+		}
+
+		delegate void AddFilesCallback(IEnumerable<TranscoderFile> files);
+		void AddFiles(IEnumerable<TranscoderFile> files) {
+			if (InvokeRequired) {
+				Invoke(new AddFilesCallback(AddFiles), files);
+				return;
+			}
+
+			foreach (var file in files) {
+				TranscoderFiles.Add(file);
+			}
+		}
+	}
+}
