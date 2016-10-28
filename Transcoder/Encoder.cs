3f24bf4f1441990a0af0e83e8ff3c5bb0cf156e6
nekno <nekno@users.noreply.github.com>
2016-10-27 19:42:23 -0500

    Added `Encoder` type.

---
+++ Transcoder/Encoder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Transcoder
+{
+	public class Encoder
+	{
+		public static Encoder QAAC = new Encoder() { FilePath = @"tools\qaac\qaac64.exe" };
+		public static Encoder FFMPEG = new Encoder() { FilePath = @"tools\ffmpeg\ffmpeg.exe" };
+
+		public String FilePath { get; protected set; }
+	}
+}
--- Transcoder/TranscoderFile.cs
+++ Transcoder/TranscoderFile.cs
@@ -11,6 +11,13 @@ namespace Transcoder
 {
 	public class TranscoderFile
 	{
+		public static Type[] Types = new Type[] {
+			Type.FLAC,
+			Type.QTAAC,
+			Type.QTALAC,
+			Type.WAV
+		};
+		
 		public String FilePath { get; protected set; }
 		public String Folder { get; protected set; }
 		public String Log { get; set; }
@@ -64,6 +71,63 @@ namespace Transcoder
 			var destRelativeFolderPath = destFolderName + srcFolderName;
 
 			Folder = destRelativeFolderPath;
+		}
+
+		public String buildCommandLineArgs(Type encoderType, Int32 bitrate, String outputFolder) {
+			return String.Format(
+				encoderType.CommandLineArgs(RequiresDecoding), 
+				FilePath, 
+				bitrate, 
+				Path.Combine(outputFolder, Folder),
+				Path.Combine(Path.Combine(outputFolder, Folder), Path.ChangeExtension(FileName, encoderType.FileExtension))
+			);
+		}
+
+		public class Type
+		{
+			public static Type QTAAC = new Type() { 
+				Name = "QuickTime AAC", 
+				FileExtension = ".m4a", 
+				Encoder = Transcoder.Encoder.QAAC, 
+				IsBitrateRequired = true,
+				CommandLineArgsWithDecoding = "- --threading --gapless-mode 2 -v{1} -o \"{3}\"",
+				CommandLineArgsWithoutDecoding = "\"{0}\" --threading --gapless-mode 2 -v{1} -d \"{2}\""
+			};
+			public static Type QTALAC = new Type() { 
+				Name = "QuickTime ALAC", 
+				Encoder = Encoder.QAAC, 
+				FileExtension = ".m4a",
+				CommandLineArgsWithDecoding = "- --threading --gapless-mode 2 -A -o \"{3}\"",
+				CommandLineArgsWithoutDecoding = "\"{0}\" --threading --gapless-mode 2 -A -d \"{2}\""
+			};
+
+			public static Type FLAC = new Type() { 
+				Name = "FLAC", 
+				Encoder = Encoder.FFMPEG, 
+				FileExtension = ".flac",
+				CommandLineArgsWithDecoding = String.Empty,
+				CommandLineArgsWithoutDecoding = "-i \"{0}\" -y \"{3}\""
+			};
+
+			public static Type WAV = new Type() { 
+				Name = "WAV",
+				Encoder = Encoder.FFMPEG, 
+				FileExtension = ".wav",
+				CommandLineArgsWithDecoding = String.Empty,
+				CommandLineArgsWithoutDecoding = "-i \"{0}\" -y \"{3}\""
+			};
+
+			public String Name { get; protected set; }
+			public String FileExtension { get; protected set; }
+			public Encoder Encoder { get; protected set; }
+			public Boolean IsBitrateRequired { get; protected set; }
+
+			protected String CommandLineArgsWithDecoding { get; set; }
+			protected String CommandLineArgsWithoutDecoding { get; set; }
+
+			public String CommandLineArgs(bool isDecodingRequired) {
+				return isDecodingRequired ? CommandLineArgsWithDecoding : CommandLineArgsWithoutDecoding;
+			}
 		}
 	}
 }
