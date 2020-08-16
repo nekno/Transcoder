5035768d2319396c2fc469f96d476637edccd33c
nekno <nekno@users.noreply.github.com>
2020-08-16 03:41:30 -0500

    Add inert encoders

--- Transcoder/Encoder.cs
+++ Transcoder/Encoder.cs
@@ -1,13 +1,23 @@
 ﻿using System;
+using System.Linq;
 
 namespace Transcoder
 {
 	public class Encoder
 	{
-		public static Encoder QAAC = new Encoder() { FilePath = @"tools\qaac\qaac64.exe" };
 		public static Encoder FFMPEG = new Encoder() { FilePath = @"tools\ffmpeg\ffmpeg.exe" };
 		public static Encoder FFPROBE = new Encoder() { FilePath = @"tools\ffmpeg\ffprobe.exe" };
+		public static Encoder NULL = new Encoder() { FilePath = String.Empty };
+		public static Encoder QAAC = new Encoder() { FilePath = @"tools\qaac\qaac64.exe" };
 
 		public String FilePath { get; protected set; }
+
+		public Boolean IsEncodingRequired
+		{
+			get
+			{
+				return !(new Encoder[] { Encoder.FFPROBE, Encoder.NULL }.Contains(this));
+			}
+		}
 	}
 }
