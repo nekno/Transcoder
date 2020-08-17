842d5e86bfb47734b05239575c9cafea01593e62
nekno <nekno@users.noreply.github.com>
2020-08-17 01:38:02 -0500

    Cleanup usings

--- Transcoder/TranscoderFile.cs
+++ Transcoder/TranscoderFile.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using MediaInfoLib;
 
 namespace Transcoder
