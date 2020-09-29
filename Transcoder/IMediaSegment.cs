98886653ac4544b09eaff2b64a0eb7283e1bc4c9
nekno <nekno@users.noreply.github.com>
2020-09-28 19:54:34 -0500

    Add IMediaSegment

---
+++ Transcoder/IMediaSegment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transcoder
+{
+    public interface IMediaSegment
+    {
+        String EndTime { get; }
+        String Name { get; }
+        Int32 Number { get; }
+        String StartTime { get; }
+    }
+}
