2521586c5707de44794dbde62cdbd7840de4e3fa
nekno <nekno@users.noreply.github.com>
2020-09-30 00:29:50 -0500

    Add BindingList.Add(IEnumerable<T>) extension method.

---
+++ Transcoder/BindingList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transcoder
+{
+    static class BindingListExtension
+    {
+        public static void Add<T>(this BindingList<T> @this, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                @this.Add(item);
+            }
+        }
+    }
+}
